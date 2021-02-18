8d32a108eb25a45995f3b64bcbcdae305778271a
mbarcon7 <marcelo.barcon@patelhs.org>
2021-02-18 17:40:02 +0000

    I started with the base of the code and already have an if/else statement.

--- Main.java
+++ Main.java
@@ -1,12 +1,23 @@
+import java.util.Scanner;
+
 class Main {
   public static void main(String[] args)
   {
+    Scanner scan = new Scanner(System.in);
 
+    hiring( )
+  }
 
+  static String hiring(String  )
+  {
+    System.out.println("How many years of experience do you have?");
+    int experience = scan.nextInt();
 
-
-
-
-
+    if(experience > 5)
+    {
+      return true;
+    }else{
+      hiring(String )
+    }
   }
 }
